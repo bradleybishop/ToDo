9b137c8b5e0ffb39c5a80b9cb9cf664b0c4a1008
Brad Bishop <bishop.bradley@gmail.com>
2016-07-28 10:30:05 -0400

    Added project file and console test.

---
+++ ToDoManager.cs
@@ -0,0 +1,11 @@
+namespace ToDoManager
+{
+  using System;
+  class ToDoManager
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to ToDo Manager");
+    }
+  }
+}
